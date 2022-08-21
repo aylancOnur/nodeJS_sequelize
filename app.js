07f054d8356e958ffedb4812c6e8d0b3b568ab19
Onur Aylanc <onuraylanc@Onurs-MacBook-Air.local>
2022-08-21 20:57:59 +0300

    sequelize with express

--- app.js
+++ app.js
@@ -7,15 +7,20 @@ const db = require("./db");
 const app = express();
 const router = express.Router();
 
-const connectToDB = async () => {
-  await db.connect();
-  // await db.createTables();
-  console.log("Done");
-};
+router.get("/getAllData", async (req, res) => {
+  try {
+    const _res = await TestModel.findAll({
+      attributes: ["id", "testName", "testSurname"],
+      logging: true,
+    });
+    res.json(_res);
+  } catch (error) {
+    res.status(500).json({ message: "Bir hata gerçekleşti!" });
+    console.log("error =>", error);
+  }
+});
 
-connectToDB();
-
-const createData = async () => {
+router.post("/createData", async (req, res) => {
   // const test = new TestModel({
   //   testName: "username",
   //   testSurname: "admin",
@@ -29,35 +34,136 @@ const createData = async () => {
   //     console.log("err =>", err.errors[0].message);
   //   });
   // return;
+  const { testName, testSurname } = req.body;
   try {
-    const res = await TestModel.create(
+    const _res = await TestModel.create(
       {
-        testName: "username6",
-        testSurname: "usersurname6",
+        testName,
+        testSurname,
       },
       { logging: true, validate: true }
     );
-    console.log("res =>", res.testFullName);
+    res.status(201).json(_res);
   } catch (error) {
+    res.status(500).json({ message: "Bir hata gerçekleşti!" });
     console.log("error =>", error.errors[0].message);
   }
-};
+});
 
-const findAllData = async () => {
+router.post("/createMultiple", async (req, res) => {
+  const { data } = req.body;
   try {
-    const res = await TestModel.findAll({
-      attributes: ["id", "testName", "testSurname"],
-      logging: true,
-    });
-    res.forEach((item) => {
-      console.log(item.dataValues);
-    });
+    const response = await TestModel.bulkCreate(data);
+    res.status(200).json(response);
+
+    console.log("removedData =>", response);
   } catch (error) {
+    res.status(500).json({ message: "Bir hata gerçekleşti!" });
     console.log("error =>", error);
   }
-};
+});
 
-const filterData = async () => {
+router.delete("/delete/:itemId", async (req, res) => {
+  console.log(req.params);
+  const { itemId } = req.params;
+  try {
+    // const findedData = await TestModel.findByPk(3);
+    const findedData = await TestModel.findOne({
+      where: { test_id: itemId },
+    });
+
+    const removedData = await findedData.destroy({
+      logging: true,
+      force: true,
+    });
+    res.status(200).json(removedData);
+    console.log("removedData =>", removedData);
+  } catch (error) {
+    res.status(500).json({ message: "Bir hata gerçekleşti!" });
+    console.log("error =>", error);
+  }
+});
+
+router.post("/findOrCreate", async (req, res) => {
+  const { testModel } = req.body;
+  try {
+    const [data, isCreated] = await TestModel.findOrCreate({
+      where: {
+        testName: testModel.testName,
+      },
+      defaults: testModel,
+    });
+
+    if (isCreated) {
+      res.status(200).json({ isExist: false, ...data });
+      return;
+    }
+    res.status(200).json({ isExist: true, ...data });
+  } catch (error) {
+    console.log("error =>", error);
+    res.status(500).json({ message: "Bir hata gerçekleşti!" });
+  }
+});
+
+router.put("/updateData/:itemId", async (req, res) => {
+  const { itemId } = req.params;
+  const { testName, testSurname } = req.body;
+  try {
+    const isUpdate = TestModel.update(
+      { testName, testSurname },
+      { where: { test_id: itemId } }
+    );
+    res.status(200).json(isUpdate);
+  } catch (error) {
+    console.log("error =>", error);
+    res.status(500).json({ message: "Bir hata gerçekleşti!" });
+  }
+});
+
+router.get("/getDataById/:itemId", async (req, res) => {
+  const { itemId } = req.params;
+
+  try {
+    const findedData = await TestModel.findByPk(itemId);
+    res.json(findedData);
+  } catch (error) {
+    res.status(500).json({ message: "Bir hata gerçekleşti!" });
+    console.log("error =>", error);
+  }
+});
+
+router.get("/getDataWithPagination", async (req, res) => {
+  const { limit, offset } = req.query;
+  console.log("req query =>", limit, offset);
+  try {
+    const response = await TestModel.findAndCountAll({
+      limit: Number(limit),
+      logging: true,
+      offset: Number(offset),
+    });
+    res.status(200).json(response);
+  } catch (err) {
+    console.log("error =>", err);
+    res.status(500).json({ message: "Bir hata gerçekleşti!" });
+  }
+});
+
+router.get("/getDataByQuery/:itemId", async (req, res) => {
+  const { itemId } = req.params;
+  const response = await db.sequelize.query(
+    "SELECT * FROM test WHERE test_id = :testId",
+    {
+      replacements: {
+        testId: itemId,
+      },
+      logging: console.log,
+      type: QueryTypes.SELECT,
+    }
+  );
+  res.status(200).json(response);
+});
+
+router.get("/filterData", async (req, res) => {
   try {
     const findedData = await TestModel.findAll({
       where: {
@@ -67,154 +173,39 @@ const filterData = async () => {
         },
       },
     });
-    console.log("finded data =>", findedData);
+    res.status(200).json(findedData);
   } catch (error) {
     console.log("error =>", error);
   }
-};
+});
 
-const deleteById = async () => {
-  try {
-    // const findedData = await TestModel.findByPk(3);
-    const findedData = await TestModel.findOne({
-      where: { test_id: 4 },
-    });
-
-    const removedData = await findedData.destroy({
-      logging: true,
-      force: true,
-    });
-    console.log("removedData =>", removedData);
-  } catch (error) {
-    console.log("error =>", error);
-  }
-};
-
-const updateData = async () => {
-  try {
-    const isUpdate = TestModel.update(
-      { testSurname: "updated surname" },
-      { where: { test_id: 2 } }
-    );
-  } catch (error) {
-    console.log("error =>", error);
-  }
-};
-
-const createMultiple = async () => {
-  try {
-    const response = await TestModel.bulkCreate([
-      {
-        testName: "username",
-        testSurname: "usersurname",
-      },
-      {
-        testName: "username2",
-        testSurname: "usersurname2",
-      },
-      {
-        testName: "username3",
-        testSurname: "usersurname3",
-      },
-      {
-        testName: "username4",
-        testSurname: "usersurname4",
-      },
-      {
-        testName: "username5",
-        testSurname: "usersurname5",
-      },
-    ]);
-  } catch (error) {
-    console.log("error =>", error);
-  }
-};
-
-const findOrCreate = async () => {
-  try {
-    const [data, isCreated] = await TestModel.findOrCreate({
-      where: {
-        testName: "Hello",
-      },
-      defaults: {
-        testName: "Hello",
-        testSurname: "World",
-      },
-    });
-    console.log("data =>", data);
-
-    if (isCreated) {
-      console.log("isCreated =>", data);
-    }
-  } catch (error) {
-    console.log("error =>", error);
-  }
-};
-
-const paginationData = async () => {
-  try {
-    const response = await TestModel.findAndCountAll({
-      limit: 2,
-      logging: true,
-      offset: 3,
-    });
-    response.rows.forEach((item) => {
-      console.log("item", JSON.stringify(item.dataValues));
-    });
-  } catch (err) {
-    console.log("error =>", err);
-  }
-};
-
-const getQueryWithSequelize = async () => {
-  const res = await db.sequelize.query(
-    "SELECT * FROM test WHERE test_id = :testId",
-    {
-      replacements: {
-        testId: 1,
-      },
-      logging: console.log,
-      type: QueryTypes.SELECT,
-    }
-  );
-  console.log("res =>", res);
-};
-
-const createWithTransaction = async () => {
+router.post("/createDataWithTransaction", async (req, res) => {
+  const { testName, testSurname } = req.body;
   const t = await db.sequelize.transaction();
   try {
-    const res = await TestModel.create(
+    const response = await TestModel.create(
       {
-        testName: "username6",
-        testSurname: "usersurname6",
+        testName,
+        testSurname,
       },
       { logging: true, validate: true }
     );
     await t.commit();
-    console.log("res =>", res.testFullName);
+    console.log("res =>", response.testFullName);
+    res.status(200).json(response);
   } catch (error) {
     await t.rollback();
     console.log("error =>", error.errors[0].message);
+    res.status(500).json({ message: "Bir hata gerçekleşti!"});
   }
-};
-
-// createData();
-// findAllData();
-// filterData();
-// deleteById();
-// updateData();
-// createMultiple();
-// findOrCreate();
-// paginationData();
-// getQueryWithSequelize();
-createWithTransaction();
-
-router.get("/", (req, res) => {
-  res.send("Hello World!");
 });
 
+app.use(express.json());
 app.use(router);
 
-app.listen(3001, () => {
+app.listen(3001, async () => {
   console.log("Server is running on port => 3001");
+  await db.connect();
+  // await db.createTables();
+  console.log("Done");
 });
